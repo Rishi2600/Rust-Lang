4a0a9246bc2562361ca73e7cc97027fbcccd274d
Rishi2600 <rishiraj6177@gmail.com>
2026-01-22 11:36:03 +0530

    basic guessinggame made: with only one guess

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
+fn main () {
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+    println!("{}", guess);
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please put in a number");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Lesser number guessed"),
+        Ordering::Greater => println!("Greater number guessed"),
+        Ordering::Equal => println!("Correct number guessed. You win!")
+    }
 }
