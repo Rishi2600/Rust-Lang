cc68f16b918e738eb4a4ffb72b659ff06c0689b6
Rishi2600 <rishiraj6177@gmail.com>
2026-01-22 11:45:48 +0530

    guessing-game: now we have a full fledged guessing game, that continues till you put in the correct guess

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -3,28 +3,31 @@ use std::io;
 use rand::Rng;
 
 fn main () {
-    println!("Guess the number");
+    println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=10);
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess");
+    loop {
+        println!("Please input your guess");
 
-    let mut guess = String::new();
-    println!("{}", guess);
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please put in a number");
+        let guess: u32 = guess.trim().parse().expect("Please put in a number");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Lesser number guessed"),
-        Ordering::Greater => println!("Greater number guessed"),
-        Ordering::Equal => println!("Correct number guessed. You win!")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Lesser number guessed"),
+            Ordering::Greater => println!("Greater number guessed"),
+            Ordering::Equal => {
+                println!("Correct number guessed. You win!");
+                break;
+            }
+        }
     }
+    
 }
