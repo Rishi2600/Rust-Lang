dd17a3b958545dd0461bf61e75f5cec3d646b636
Rishi2600 <rishiraj6177@gmail.com>
2026-01-23 23:31:36 +0530

    Revising the guessing game from scratch

--- guessing-game/src/revise.rs
+++ guessing-game/src/revise.rs
@@ -1,13 +1,35 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main () {
     println!("Guess the Number");
 
-    println!("PUt in the number");
+    let random_numebr = rand::thread_rng().gen_range(1..=10);
 
-    let mut guess = String::new();
+    println!("The random numer is {random_number}");
 
-    io::stdin().read_line(&mut guess).expect("Failed to read the line");
-
-    println!("You guessed: {guess}");
+    loop {
+        println!("PUt in the number");
+    
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess).expect("Failed to read the line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(value) => value,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::less => println!("Number guessed is lesser"),
+            Ordering::Greater => println!("Number guessed is greater"),
+            Ordering::Equal => {
+                println!("Correct Guess. You Win!");
+                break:
+            }
+        };
+    }
 }
