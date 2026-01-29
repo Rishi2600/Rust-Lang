e5f23b51fb4601aaabb8c0ed49287dafb3090d24
Rishi2600 <rishiraj6177@gmail.com>
2026-01-30 00:03:04 +0530

    global vs local scope

--- ch-1/shadowing/src/main.rs
+++ ch-1/shadowing/src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
     let x = 5;
 
-    let x = x + 1;
+    let x = x + 100;
 
     {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
+        let x = x + 1;
+
+        {
+            let x = x * 2;
+            println!("The value of x in thesecond  inner scope is: {x}");
+        }
+
+        println!("The value of x in first inner scope is: {x}");
     }
 
     println!("The value of x is: {x}");
