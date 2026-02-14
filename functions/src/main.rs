0cdd4d0b2f13618ecbb17af63000da747c292031
Rishi2600 <rishiraj6177@gmail.com>
2026-02-14 22:54:16 +0530

    funciton: rust does not care about where the function is defined but, It should be called inside the scope

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
-}
+    
+    fn another_function() {
+        println!("Another function.");
+    }
 
-fn another_function() {
-    println!("Another function.");
+    another_function();
 }
