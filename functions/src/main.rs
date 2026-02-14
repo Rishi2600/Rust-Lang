756e1d73fd0da4a195e4c5879d85f7681ac8943c
Rishi2600 <rishiraj6177@gmail.com>
2026-02-14 22:59:16 +0530

    Parameters and arguments: these tells us the differecne between the parameter and an argument - teh defined variable is parameter and the values to those variables are changed based on arguments

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(5);
+}
 
-    
-    fn another_function() {
-        println!("Another function.");
-    }
-
-    another_function();
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
 }
