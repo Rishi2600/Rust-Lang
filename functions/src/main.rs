d3a15c4ad3745f9459aee9494517bbcc35d2b2f3
Rishi2600 <rishiraj6177@gmail.com>
2026-02-17 23:20:37 +0530

    defining a fn is a statement but calling the fn is not a statement as it might return some values; statements do not returns values

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,18 +2,3 @@ fn main() {
     let final_result = add_numbers(3, 4);
     println!("{}", final_result);
 }
-
-fn add_numbers(x: i32, y: i32) -> i32 {
-    // --- This is a STATEMENT ---
-    // It performs an action (printing) but does not return a value.
-    println!("Adding numbers!");
-
-    // --- This is a STATEMENT ---
-    // It performs an action (creating a variable).
-    let result = x + y;
-
-    // --- This is STATEMENT not an EXPRESSION ---
-    // since It has a semicolon, now it behanves like another statement. It evaluates to the value of `result`.
-    // This value is now not returned from the function.
-    result;
-}
