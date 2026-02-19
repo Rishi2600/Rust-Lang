5ea26dfb8272dc756c147a0de83b8098cbe79dca
Rishi2600 <rishiraj6177@gmail.com>
2026-02-19 23:21:05 +0530

    here, else is the fallback to the closest if and not the global fallback, to have a global fallback we need to follow the else if block convention

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -8,8 +8,10 @@ fn main() {
     if number % 3 == 0 {
         println!("number is divisible by 3");
     }
-
+    
     if number % 2 == 0 {
         println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
