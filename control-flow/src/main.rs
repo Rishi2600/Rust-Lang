b508114025724a63c304b615bec8fed7bfb4230b
Rishi2600 <rishiraj6177@gmail.com>
2026-02-20 23:30:03 +0530

    if in the let statement

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,23 +1,6 @@
 fn main() {
-    let mut found = false;
-    let number = 6;
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
 
-    if number % 4 == 0 {
-        println!("number is devisible by 4");
-        found = true;
-    }
-
-    if number % 3 == 0 {
-        println!("number is devisible by 3");
-        found = true;
-    }
-
-    if number % 2 == 0 {
-        println!("number is devisible by 2");
-        found = true;
-    }
-
-    if found == false {
-        println!("number is not devisible by either 2, 3, or 4");
-    }
+    println!("The value of number is: {number}");
 }
