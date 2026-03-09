cbfbebcd86ebeec7b8da3a5498000786f510fea4
Rishi2600 <rishiraj6177@gmail.com>
2026-03-09 18:02:18 +0000

    pattern matching with range

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
-    let magic = "nuf si tsuR".chars().rev().collect::<String>();
-    println!("{}", magic); // "Rust is fun"
+    let secret_char = 'g';
+
+    match secret_char {
+        'a'..='m' => println!("First half of the alphabet!"),
+        'n'..='z' => println!("Second half of the alphabet!"),
+        _ => println!("Not a lowercase letter!"),
+    }
 }
