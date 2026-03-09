47ea6842451b2848be3d73787dca9af7d76ada40
Rishi2600 <rishiraj6177@gmail.com>
2026-03-09 18:04:02 +0000

    smart array filtering

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    let secret_char = 'g';
-
-    match secret_char {
-        'a'..='m' => println!("First half of the alphabet!"),
-        'n'..='z' => println!("Second half of the alphabet!"),
-        _ => println!("Not a lowercase letter!"),
-    }
+    // An infinite range of squares, but we only take the first 5
+    let squares: Vec<_> = (1..).map(|x| x * x).take(5).collect();
+    
+    println!("{:?}", squares); // [1, 4, 9, 16, 25]
 }
