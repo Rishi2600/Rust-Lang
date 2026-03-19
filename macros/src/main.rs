01906af32890e0b07a27ebe513e9fcc43e1ac49d
Rishi2600 <rishiraj6177@gmail.com>
2026-03-19 17:24:57 +0000

    declarative macro: add

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+macro_rules! add{
+ // macth like arm for macro
+    ($a:expr,$b:expr)=>{
+ // macro expand to this code
+        {
+// $a and $b will be templated using the value/variable provided to macro
+            $a+$b
+        }
+    }
+}
+
+fn main(){
+ // call to macro, $a=1 and $b=2
+    let add = add!(1,2);
+    println!("{}", add);
 }
