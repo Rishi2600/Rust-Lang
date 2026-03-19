0d370b6988a47a65dbabf24ca723ff56e467e41b
Rishi2600 <rishiraj6177@gmail.com>
2026-03-19 17:42:57 +0000

    declarative macro: type as an argument

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,29 +1,10 @@
-macro_rules! add{
- // first arm match add!(1,2), add!(2,3) etc
-    ($a:expr,$b:expr)=>{
-        {
-            $a+$b
-        }
-    };
-// Second arm macth add!(1), add!(2) etc
-    ($a:expr)=>{
-        {
-            $a
-        }
+macro_rules! add_as{
+// using a ty token type for macthing datatypes passed to maccro
+    ($a:expr,$b:expr,$typ:ty)=>{
+        $a as $typ + $b as $typ
     }
 }
 
 fn main(){
-    let x=0;
-    let arm1 = add!(1,2);
-    println!("{}",  arm1);
-    let arm2 = add!(x);
-    println!("{}", arm2);
-
-    let final_sum = arm1 + arm2;
-
-    println!("sum of both arms: {}", final_sum);
+    println!("{}",add_as!(0,2,u8));
 }
-
-/*There can be multiple branches in a single macro expanding to different code based on different arguments.
-Each branch can take multiple arguments, starting with the $ sign and followed by a token type:*/
