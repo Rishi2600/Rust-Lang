003ede2db8abd3084eecfc8e3e33b46b93b1e3a1
Rishi2600 <rishiraj6177@gmail.com>
2026-03-20 19:45:10 +0000

    declarative macros: make_public

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,33 +1,31 @@
-macro_rules! ok_or_return{
- // internal rule.
-    (@error $a:ident,$($b:tt)* )=>{
-        {
-        match $a($($b)*) {
-            Ok(value)=>value,
-            Err(err)=>{
-                return Err(err);
+macro_rules! make_public{
+    (
+     $(#[$meta:meta])* 
+     $vis:vis struct $struct_name:ident {
+        $(
+        $(#[$field_meta:meta])*
+        $field_vis:vis $field_name:ident : $field_type:ty
+        ),*$(,)+
+    }
+    ) => {
+
+            $(#[$meta])*
+            pub struct $struct_name{
+                $(
+                $(#[$field_meta:meta])*
+                pub $field_name : $field_type,
+                )*
             }
-        }
-        }
-    };
-
-// public rule can be called by the user.
-    ($a:ident($($b:tt)*))=>{
-        ok_or_return!(@error $a,$($b)*)
-    };
-}
-
-fn some_work(i:i64,j:i64)->Result<(i64,i64),String>{
-    if i+j>2 {
-        Ok((i,j))
-    } else {
-        Err("error".to_owned())
     }
 }
 
-fn main()->Result<(),String>{
-   // instead of round bracket curly brackets can also be used
-    ok_or_return!{some_work(1,4)};
-    ok_or_return!(some_work(1,0));
-    Ok(())
+fn main(){
+    make_public!{
+        #[derive(Debug)]
+        struct Name{
+            n:i64,
+            t:i64,
+            g:i64,
+        }
+    }
 }
