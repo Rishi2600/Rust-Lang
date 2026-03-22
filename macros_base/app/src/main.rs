731f91d6e826fdf332838837f5021490ad1423d6
Rishi2600 <rishiraj6177@gmail.com>
2026-03-22 19:03:26 +0000

    corgo-expand dependency added

--- macros_base/app/src/main.rs
+++ macros_base/app/src/main.rs
@@ -1,23 +1,12 @@
-use my_macros::Describe;
 use my_macros::log_call;
 
-// THE MISSING PIECE: The trait definition
-pub trait Describe {
-    fn describe(&self);
-}
-
-#[derive(Describe)]
-struct User {
-    #[allow(dead_code)]
-    name: String,
-}
-
 #[log_call]
-fn do_work() {
-    let u = User { name: "Alice".into() };
-    u.describe();
+fn add_numbers(a: i32, b: i32) -> i32 {
+    println!("Calculating...");
+    a + b
 }
 
 fn main() {
-    do_work();
+    let sum = add_numbers(10, 20);
+    println!("Result: {}", sum);
 }
--- macros_base/my_macros/src/lib.rs
+++ macros_base/my_macros/src/lib.rs
@@ -1,40 +1,33 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, ItemFn};
+use syn::{parse_macro_input, ItemFn};
 
-// --- The Derive Macro ---
-#[proc_macro_derive(Describe)]
-pub fn describe_derive(input: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(input as DeriveInput);
-    let name = &ast.ident;
-
-    let expanded = quote! {
-        // This implements the trait we define in main.rs
-        impl Describe for #name {
-            fn describe(&self) {
-                println!("I am a struct named {}", stringify!(#name));
-            }
-        }
-    };
-    TokenStream::from(expanded)
-}
-
-// --- The Attribute Macro ---
 #[proc_macro_attribute]
 pub fn log_call(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // 1. Parse the function we're wrapping
     let input = parse_macro_input!(item as ItemFn);
+    
+    // 2. Extract parts of the function
     let name = &input.sig.ident;
-    let block = &input.block;
     let vis = &input.vis;
     let sig = &input.sig;
+    let block = &input.block;
 
+    // 3. Generate the "Wrapper"
+    // We recreate the function signature and put our logic inside
     let expanded = quote! {
         #vis #sig {
-            println!("--- Entering: {} ---", stringify!(#name));
+            println!(">> Entering: {}", stringify!(#name));
+            
+            // Execute the original block of code
             let result = { #block };
-            println!("--- Exiting: {} ---", stringify!(#name));
+            
+            println!("<< Exiting: {}", stringify!(#name));
+            
+            // Return the result of the block
             result
         }
     };
+
     TokenStream::from(expanded)
 }
