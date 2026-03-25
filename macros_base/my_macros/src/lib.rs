7078f84b90603e837999d832f02bbaf4cd286768
Rishi2600 <rishiraj6177@gmail.com>
2026-03-25 16:43:31 +0000

    proc macros: we framework handler

--- macros_base/my_macros/src/lib.rs
+++ macros_base/my_macros/src/lib.rs
@@ -1,26 +1,38 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, ItemFn, LitStr, FnArg, Pat};
 
 #[proc_macro_attribute]
-pub fn trace(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input_fn = parse_macro_input!(item as ItemFn);
-    
-    let fn_name = &input_fn.sig.ident;
-    let fn_vis = &input_fn.vis;
-    let fn_args = &input_fn.sig.inputs;
-    let fn_block = &input_fn.block;
-    let fn_ret = &input_fn.sig.output;
+pub fn get_page(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // 1. Parse the Attribute (DI: Database URL)
+    let db_url = parse_macro_input!(attr as LitStr);
 
+    // 2. Parse the Function (CLI: Argument Introspection)
+    let input_fn = parse_macro_input!(item as ItemFn);
+    let fn_name = &input_fn.sig.ident;
+    let fn_block = &input_fn.block;
+    
+    // Get the name of the first argument (e.g., "user_id")
+    let arg_name = if let Some(FnArg::Typed(pat_type)) = input_fn.sig.inputs.first() {
+        if let Pat::Ident(ref id) = *pat_type.pat {
+            id.ident.to_string()
+        } else { "id".into() }
+    } else { "id".into() };
+
+    // 3. Reconstruct (The Grand Summary)
     let expanded = quote! {
-        #fn_vis fn #fn_name(#fn_args) #fn_ret {
-            println!("[TRACE] Entering {}...", stringify!(#fn_name));
+        fn #fn_name(raw_input: i32) -> String {
+            // Internal Logic (Project 4: DI)
+            let db_connection = #db_url;
+            let #fn_name = #arg_name; // Mapping input to user's variable name
             
-            // We use a block to capture the return value if there is one
-            let result = (|| #fn_block)();
-            
-            println!("[TRACE] Exiting {}...", stringify!(#fn_name));
-            result
+            println!("--- LOG: Fetching {} from {} ---", #arg_name, db_connection);
+
+            // Execute the user's logic (Project 1 & 2: DSL/Mapper)
+            let body: String = (|| #fn_block)();
+
+            // Project 1: Wrapping in a final HTML template
+            format!("<html><body>{}</body></html>", body)
         }
     };
 
