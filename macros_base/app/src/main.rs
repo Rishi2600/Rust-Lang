1a3402b8a5f5baeb7539ee3b03fd240be976b8ee
Rishi2600 <rishiraj6177@gmail.com>
2026-03-25 16:43:50 +0000

    proc macros: we framework handler

--- macros_base/app/src/main.rs
+++ macros_base/app/src/main.rs
@@ -1,11 +1,21 @@
-use my_macros::trace;
+use my_macros::get_page;
 
-#[trace]
-fn add_numbers(a: i32, b: i32) -> i32 {
-    a + b
+#[get_page("postgres://localhost:5432")]
+fn user_profile(user_id: i32) -> String {
+    // Imagine this came from our SQL Mapper Project
+    let username = "Rishi_The_Macro_Master";
+    
+    // Imagine this used our HTML DSL Project
+    format!(
+        "<h1>Profile for User #{}</h1><p>Username: {}</p>", 
+        user_id, username
+    )
 }
 
 fn main() {
-    let sum = add_numbers(5, 10);
-    println!("The result is: {}", sum);
+    // We simulate a web request coming in for user 42
+    let response = user_profile(42);
+    
+    println!("\nFinal Rendered Page:");
+    println!("{}", response);
 }
