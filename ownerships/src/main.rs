5255a4dc8a4be133269285964257ecf32183c992
Rishi2600 <rishiraj6177@gmail.com>
2026-03-28 19:27:39 +0000

    ownership: moving the data instead of creating a copy

--- ownerships/src/main.rs
+++ ownerships/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("This is to learn memory management in rust that is handled by ownerships");
+    let s1 = String::from("The Ancient Scroll");
+    
+    // Ownership of the data MOVES from s1 to s2
+    let s2 = s1; 
+
+    // println!("{}", s1); // ❌ ERROR: s1 is now "empty" (invalid)
+    println!("{}", s2);    
 }
