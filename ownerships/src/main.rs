86161de8f787164ec4264506473d6930a62deeaa
Rishi2600 <rishiraj6177@gmail.com>
2026-03-28 19:28:28 +0000

    ownership: reference

--- ownerships/src/main.rs
+++ ownerships/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let s1 = String::from("The Ancient Scroll");
-    
-    // Ownership of the data MOVES from s1 to s2
-    let s2 = s1; 
+    let s1 = String::from("Library Book");
 
-    // println!("{}", s1); 
-    println!("{}", s2);    
+    // We pass a reference (&), not the value itself
+    print_length(&s1); 
+
+    // s1 still owns the data, so we can use it here!
+    println!("I still have the '{}'", s1);
+}
+
+fn print_length(s: &String) {
+    println!("The book is {} chars long", s.len());
 }
