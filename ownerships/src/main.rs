703caa938f7ce0c946fc21c47a1d36de9a15116c
Rishi2600 <rishiraj6177@gmail.com>
2026-03-28 19:32:00 +0000

    ownership: lifeline or time-based ownership

--- ownerships/src/main.rs
+++ ownerships/src/main.rs
@@ -1,15 +1,16 @@
-fn main() {
-    let mut document = String::from("Draft 1");
+struct Wizard<'a> {
+    name: &'a str, // This wizard is "anchored" to a string slice
+}
 
-    let reader1 = &document;
-    let reader2 = &document; // ✅ Fine: multiple readers
+fn main() {
+    let name = String::from("Gandalf");
+    let wiz;
+
+    {
+        let _temporary_name = String::from("Saruman");
+        wiz = Wizard { name: &name }; 
+        // Because 'temporary_name' dies at the end of this brace.
+    }
     
-    // let writer = &mut document; // ❌ ERROR: Cannot edit while readers are looking
-    
-    println!("Readers see: {} and {}", reader1, reader2);
-    
-    // Now that readers are "done" (out of scope), we can edit
-    let writer = &mut document;
-    writer.push_str(" - Final Version");
-    println!("{}", writer);
+    println!("{}", wiz.name); 
 }
