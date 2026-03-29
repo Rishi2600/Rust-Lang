f0a1f511cac91b068061b596bc831b72df33ab7e
Rishi2600 <rishiraj6177@gmail.com>
2026-03-29 19:16:36 +0000

    ownership: move partial struct

--- ownerships/src/main.rs
+++ ownerships/src/main.rs
@@ -1,16 +1,17 @@
-struct Wizard<'a> {
-    name: &'a str, // This wizard is "anchored" to a string slice
+struct Golem {
+    name: String,
+    power_source: String,
 }
 
 fn main() {
-    let name = String::from("Gandalf");
-    let wiz;
+    let my_golem = Golem {
+        name: String::from("Stone Sentinel"),
+        power_source: String::from("Ancient Battery"),
+    };
 
-    {
-        let _temporary_name = String::from("Saruman");
-        wiz = Wizard { name: &name }; 
-        // Because 'temporary_name' dies at the end of this brace.
-    }
-    
-    println!("{}", wiz.name); 
+    // We MOVE the name out, but leave the power_source
+    let _name_only = my_golem.name;
+
+    // println!("{}", my_golem.name);
+    println!("Power source still here: {}", my_golem.power_source);
 }
