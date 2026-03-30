c7ebdb113790152fc3b9fcf81ab071355a99ec30
Rishi2600 <rishiraj6177@gmail.com>
2026-03-30 16:15:24 +0000

    pattern matching: choice

--- pattern_match/src/main.rs
+++ pattern_match/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+enum Portal {
+    Fire,
+    Ice,
+    Void,
+}
+
+fn main() {
+    let destination = Portal::Ice;
+
+    match destination {
+        Portal::Fire => println!("Bring a fan."),
+        Portal::Ice => println!("Bring a coat."),
+        Portal::Void => println!("Bring... nothing?"), // Must cover all cases!
+    }
+}
