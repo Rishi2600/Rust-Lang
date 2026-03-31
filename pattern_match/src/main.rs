25ba7a11c0cc96848a36226b62d584baf86768d6
Rishi2600 <rishiraj6177@gmail.com>
2026-03-31 17:57:26 +0000

    pattern matching: binding rename

--- pattern_match/src/main.rs
+++ pattern_match/src/main.rs
@@ -1,11 +1,19 @@
-fn main() {
-    let system_power = true;
-    let authorization_level = 10;
+enum Robot {
+    Worker { id: u32, task: String },
+}
 
-    // We match on the PAIR of values
-    match (system_power, authorization_level) {
-        (true, 10) => println!("Full Access: Critical Systems Online."),
-        (true, _)  => println!("Limited Access: Systems Online."),
-        (false, _) => println!("Systems Offline. Emergency power only."),
+fn main() {
+    let bot = Robot::Worker { id: 777, task: String::from("Welding") };
+
+    match bot {
+        // 'entire_bot' binds to the whole Worker, while 'id' binds to the 777
+        entire_bot @ Robot::Worker { id: 700..=800, .. } => {
+            println!("Bot in the 700-series detected!");
+            // We can still use 'entire_bot' here as the full object
+            inspect_robot(entire_bot); 
+        }
+        _ => (),
     }
 }
+
+fn inspect_robot(r: Robot) { /* ... */ }
