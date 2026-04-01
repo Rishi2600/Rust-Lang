e9836bee0528af31a5387c88938b9bae862611ff
Rishi2600 <rishiraj6177@gmail.com>
2026-04-01 17:25:44 +0000

    pattern matching: ref & mut ref

--- pattern_match/src/main.rs
+++ pattern_match/src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
-    let hand = [10, 2, 5, 8, 10];
+    let mut name = Some(String::from("Rishi"));
 
-    match hand {
-        [10, .., 10] => println!("A pair of tens at the ends!"),
-        [first, second, ..] => println!("Started with {} and {}", first, second),
-        [] => println!("Empty hand."),
+    match name {
+        // 'ref mut n' says: "Don't move the string, just give me a mutable pointer to it"
+        Some(ref mut n) => {
+            n.push_str(" the Architect");
+            println!("Modified: {}", n);
+        },
+        None => (),
     }
+
+    // Because we used 'ref mut', 'name' is still valid here!
+    println!("Original is still alive: {:?}", name);
 }
