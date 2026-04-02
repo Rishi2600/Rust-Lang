428f92c449c786c942b6cbfb3942f7678656ba7a
Rishi2600 <rishiraj6177@gmail.com>
2026-04-02 16:59:07 +0000

    traits: iterator

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,16 +1,12 @@
-struct Seconds(u32);
-struct Minutes(u32);
-
-impl From<Minutes> for Seconds {
-    fn from(m: Minutes) -> Self {
-        Seconds(m.0 * 60)
-    }
-}
-
 fn main() {
-    let mins = Minutes(5);
-    
-    // We can use .into() because we implemented From!
-    let secs: Seconds = mins.into(); 
-    println!("Total seconds: {}", secs.0);
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    // This "Lazy" chain does nothing until .collect() is called
+    let result: Vec<i32> = numbers.into_iter()
+        .filter(|x| x % 2 != 0) // Keep odds
+        .map(|x| x * x)         // Square them
+        .take(2)                // Only take the first two
+        .collect();             // Turn back into a Vec
+
+    println!("{:?}", result); // [1, 9]
 }
