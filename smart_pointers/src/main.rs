9724953ee7336647de756c2cedd3612c30aef6b2
Rishi2600 <rishiraj6177@gmail.com>
2026-04-04 12:59:38 +0000

    smart pointer: Vec

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    // 5 lives on the Stack
-    let x = 5; 
+    let mut v: Vec<i32> = Vec::with_capacity(2);
 
-    // 5 lives on the Heap, 'y' is a pointer on the Stack
-    let y = Box::new(5); 
-
-    println!("y is {}", y); 
-} // When 'y' goes out of scope, the Heap memory is automatically freed.
+    for i in 0..10 {
+        println!(
+            "Item {}: Address: {:p}, Len: {}, Cap: {}", 
+            i, v.as_ptr(), v.len(), v.capacity()
+        );
+        v.push(i);
+    }
+}
