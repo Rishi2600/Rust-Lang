cf2a2bf9a48c4f15b616f559b874e6575383a10c
Rishi2600 <rishiraj6177@gmail.com>
2026-04-04 12:58:59 +0000

    smart pointer: Box

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
-}
+    // 5 lives on the Stack
+    let x = 5; 
+
+    // 5 lives on the Heap, 'y' is a pointer on the Stack
+    let y = Box::new(5); 
+
+    println!("y is {}", y); 
+} // When 'y' goes out of scope, the Heap memory is automatically freed.
