46f57bfdc4bf27c678e3e575767043b1592d185e
Rishi2600 <rishiraj6177@gmail.com>
2026-04-07 17:35:57 +0000

    hashmap

--- security/src/main.rs
+++ security/src/main.rs
@@ -1,3 +1,15 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut scores = HashMap::new();
+    let text = "apple banana apple orange apple banana";
+
+    for word in text.split_whitespace() {
+        // .entry() returns an Enum! 
+        // If it's Vacant, it inserts 0. Then it returns a mutable reference to the value.
+        let count = scores.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", scores); // {"apple": 3, "banana": 2, "orange": 1}
 }
