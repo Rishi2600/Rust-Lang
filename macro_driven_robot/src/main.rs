f4cb6ecd900fc2e867cbb2b73e671b55dd6d7e92
Rishi2600 <rishiraj6177@gmail.com>
2026-04-10 05:39:59 +0000

    main orchestrator

--- macro_driven_robot/src/main.rs
+++ macro_driven_robot/src/main.rs
@@ -1,3 +1,40 @@
+mod robot;
+mod commands;
+
+use robot::Robot;
+use commands::{Action, MoveForward, Speak};
+
+// The Macro: It recursively parses the DSL into Boxed Trait Objects
+macro_rules! robot_program {
+    // Match 'move_forward'
+    (move_forward $dist:expr; $($rest:tt)*) => {
+        queue.push(Box::new(MoveForward($dist)));
+        robot_program!($($rest)*);
+    };
+    // Match 'speak'
+    (speak $msg:expr; $($rest:tt)*) => {
+        queue.push(Box::new(Speak($msg.to_string())));
+        robot_program!($($rest)*);
+    };
+    // Base case: No more commands
+    () => {};
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut r2d2 = Robot::new("R2-D2");
+    let mut queue: Vec<Box<dyn Action>> = Vec::new();
+
+    // Using our custom DSL!
+    robot_program! {
+        move_forward 10;
+        speak "Beep Boop!";
+        move_forward 5;
+    }
+
+    // Execute the compiled queue
+    for cmd in queue {
+        cmd.execute(&mut r2d2);
+    }
+
+    r2d2.status();
 }
