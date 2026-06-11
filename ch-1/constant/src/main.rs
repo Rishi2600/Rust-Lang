ade518d1733da6af8194ce496fb4a7f65503fc21
Rishi2600 <rishiraj6177@gmail.com>
2026-06-11 20:40:45 +0530

    extension trait

--- ch-1/constant/src/main.rs
+++ ch-1/constant/src/main.rs
@@ -1,27 +1,17 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
+// 1. Define a brand new trait
+pub trait IsEven {
+    fn is_even(&self) -> bool;
+}
+
+// 2. Implement your trait for an existing type from the Standard Library
+impl IsEven for i32 {
+    fn is_even(&self) -> bool {
+        self % 2 == 0
+    }
+}
 
 fn main() {
-    let counter = Arc::new(Mutex::new(0));
-    let counter_clone = Arc::clone(&counter);
-
-    // This thread will intentionally crash while holding the lock
-    let handle = thread::spawn(move || {
-        let mut data = counter_clone.lock().unwrap();
-        *data = 42;
-        panic!("Thread crashed abruptly!"); 
-    });
-
-    let _ = handle.join(); // Let the thread finish crashing
-
-    // Trying to access the data from the main thread
-    match counter.lock() {
-        Ok(data) => println!("Counter data: {}", *data),
-        Err(poisoned) => {
-            println!("⚠️ Warning: The mutex was poisoned by a panicked thread!");
-            // You can choose to recover the data anyway if you need to
-            let recovered_data = poisoned.into_inner();
-            println!("Recovered data safely: {}", *recovered_data);
-        }
-    }
+    let number = 42;
+    // Magic: i32 now natively has the .is_even() method!
+    println!("Is {} even? {}", number, number.is_even());
 }
