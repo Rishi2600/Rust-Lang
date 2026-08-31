75971948edc09338748cd4158bcf27f6a8419a1d
Rishi2600 <rishiraj6177@gmail.com>
2026-08-31 23:33:04 +0530

    Phantom Custom Allocator

--- ch-1/constant/src/main.rs
+++ ch-1/constant/src/main.rs
@@ -1,14 +1,35 @@
-// Requires the `tikv-jemallocator` crate in Cargo.toml
-// use tikv_jemallocator::Jemalloc;
+use std::marker::PhantomData;
 
-// #[global_allocator]
-// static GLOBAL: Jemalloc = Jemalloc;
+// Marker types representing physical memory banks
+pub struct GpuMemory;
+pub struct SystemRam;
+
+// A pointer tagged with its memory domain
+pub struct MemoryBlock<Domain> {
+    pub address: usize,
+    pub size: usize,
+    _domain: PhantomData<Domain>, // Erasable type tag (0 bytes!)
+}
+
+fn allocate_gpu_buffer(size: usize) -> MemoryBlock<GpuMemory> {
+    MemoryBlock {
+        address: 0xDEAD_BEEF, // Simulated GPU VRAM location
+        size,
+        _domain: PhantomData,
+    }
+}
+
+fn process_gpu_data(block: &MemoryBlock<GpuMemory>) {
+    println!("Processing {} KB in VRAM at address 0x{:X}", block.size, block.address);
+}
 
 fn main() {
-    // All allocations here automatically use the custom high-throughput allocator
-    let mut data = Vec::with_capacity(1_000_000);
-    for i in 0..1_000_000 {
-        data.push(i);
-    }
-    println!("Allocated {} items using custom global allocator.", data.len());
+    let vram_block = allocate_gpu_buffer(1024);
+    
+    process_gpu_data(&vram_block);
+
+    // If you try to pass System RAM to a GPU function:
+    // let ram_block: MemoryBlock<SystemRam> = ...;
+    // process_gpu_data(&ram_block); 
+    // ❌ COMPILE ERROR: Expected `MemoryBlock<GpuMemory>`, found `MemoryBlock<SystemRam>`
 }
